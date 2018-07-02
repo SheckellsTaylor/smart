d0457963f2c2a816970273b31ddad961ca8dfafc
Taylor Sheckells <31837227+SheckellsTaylor@users.noreply.github.com>
2018-07-02 03:15:08 -0400

    change posts table

--- database/migrations/2018_03_04_003755_create_posts_table.php
+++ database/migrations/2018_03_04_003755_create_posts_table.php
@@ -18,9 +18,9 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->string('category');
-            $table->LONGBLOB('video');
+            $table->string('video');
             $table->string('duration');
-            $table->LONGBLOB('image');
+            $table->string('image');
             $table->text('metaauth');
             $table->text('metades');
             $table->text('metakey');
